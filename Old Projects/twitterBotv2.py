2927828b36b87559f5efe5f5a9ce683aa2f8d43a
Dhruv Mantri <59103725+Dhruv-Mantri@users.noreply.github.com>
2024-02-22 16:02:20 -0700

    removed personal data twitterBotv2.py

--- Old Projects/twitterBotv2.py
+++ Old Projects/twitterBotv2.py
@@ -1,15 +1,15 @@
 import tweepy
 
-api_key = "4lmp6WTg3SHWI7d6KPTAHFJyT"
-api_key_secret = "cSFNdipcDUlKhue3ZkIeLxVDUhMrb5MWpbEihXSBorKG9x6MaN"
-access_token = "1449900639689998336-Uzeho8Wd89KLXge3cCXE7yMuPL26fB"
-access_token_secret = "gmMyOSCWXPXHGZrv352FjGR6ecLOOFsHKYPDXB8K0e2yA"
+api_key = "[insert API key]"
+api_key_secret = "[insert secret API key]"
+access_token = "[insert access token]"
+access_token_secret = "[insert secret access token]"
 
 authenticator = tweepy.OAuthHandler(api_key, api_key_secret)
 authenticator.set_access_token(access_token, access_token_secret)
 
 api = tweepy.Client(consumer_key= api_key,consumer_secret= api_key_secret,access_token= access_token,access_token_secret= access_token_secret)
 
-creator = api.get_user(username= "MasterDhruv1")
+creator = api.get_user(username= "[your username]")
 # api.follow()
 print(creator.name)
